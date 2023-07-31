f29380035da29948c7069535fab7fc483281a4c6
andresm220 <123190520+andresm220@users.noreply.github.com>
2023-07-30 22:08:40 -0600

    Juego casi terminado (faltan sonidos y el power up)

---
+++ Pixel-Race-Project-main-copy/GameOverActor.java
@@ -0,0 +1,8 @@
+import greenfoot.*;
+
+public class GameOverActor extends Actor {
+    public GameOverActor() {
+        GreenfootImage gameOverImage = new GreenfootImage("Game_over.png");
+        setImage(gameOverImage);
+    }
+}
---
+++ Pixel-Race-Project-main-copy/pista.java
@@ -0,0 +1,138 @@
+import greenfoot.*;
+import java.util.List;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pista here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pista extends World
+{
+    private contador puntuacion;
+    private contador nivel;
+    
+    private int velo;
+    private int num_carros_rebasados;
+    private int rebaso_requerido;
+    private Carro rojo;
+    private int obstaculos;
+    
+    
+    
+    
+
+    /**
+     * Constructor for objects of class pista.
+     * 
+     */
+    public pista()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 700, 1); 
+        prepare();
+        num_carros_rebasados= 0;
+        rebaso_requerido= 2;
+        velo= 3;
+        puntuacion= new contador("Puntos: ");
+        nivel= new contador("Nivel: ");
+        nivel.add(1);
+        rojo= new Carro(velo);
+        addObject(rojo,300,600);
+        addObject(puntuacion,80,90);
+        addObject(nivel,80,60);
+        
+        
+        
+        
+        
+    }
+    public void act(){
+        subir_nivel();
+        añadir_obstaculos();
+        
+    }
+    public int numeroRandom(int inicio,int fin){
+      int norm= Greenfoot.getRandomNumber(fin-inicio + 1);
+      return norm + inicio;
+    }
+    public void subir_puntos(int num){
+        puntuacion.add(num);
+    }
+    public void subir_rebasos(){
+        num_carros_rebasados++;
+    }
+    public void reducir_rivales(){
+        obstaculos--;
+    }
+    public void subir_nivel(){
+        if(num_carros_rebasados == rebaso_requerido){
+            num_carros_rebasados=0;
+            rebaso_requerido= rebaso_requerido + 2;
+            velo++;
+            nivel.add(1);
+            rojo.subir_velo();
+            
+        }
+    }
+    public void añadir_obstaculos(){
+        if (obstaculos == 0){
+            int carril = numeroRandom(0,3);
+            if (carril ==0){
+                addObject(new barril (velo),180,80);
+                   
+
+                
+            }
+            else if (carril==1){
+                addObject(new barril (velo),290,80);  
+             
+            }
+            else{
+                addObject(new barril (velo),410,80);
+                   
+                     
+            }
+            carril++;
+            carril = carril %3;
+            
+            if (carril ==0){
+                addObject(new barril(velo),180,80);
+                   
+
+                
+            }
+            else if (carril==1){
+                addObject(new barril (velo),290,80); 
+    
+             
+            }
+            else{
+                addObject(new barril (velo),410,80);
+                   
+                     
+            }
+            
+            
+            obstaculos = 2;
+            
+          
+            
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+}
